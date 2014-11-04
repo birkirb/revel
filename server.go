90edc437c0117ec172dd4b1497390d8e8e6ea72b
Birkir A. Barkarson <birkirb@stoicviking.net>
2014-11-05 00:14:53 +0100

    Use goagain for signal handling and fork/spawn.
    - Add shutdown hooks for per app handling.
    - Wait group for server handler to ensure all requests are completed.
    - Still missing TLS refactoring.
    - Only use for the server, not the harness.

--- server.go
+++ server.go
@@ -1,14 +1,17 @@
 package revel
 
 import (
-	"code.google.com/p/go.net/websocket"
-	"fmt"
 	"io"
 	"net"
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
+
+	"github.com/rcrowley/goagain"
+
+	"code.google.com/p/go.net/websocket"
 )
 
 var (
@@ -16,6 +19,7 @@ var (
 	MainTemplateLoader *TemplateLoader
 	MainWatcher        *Watcher
 	Server             *http.Server
+	wg                 sync.WaitGroup
 )
 
 // This method handles all requests.  It dispatches to handleInternal after
@@ -33,6 +37,8 @@ func handle(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleInternal(w http.ResponseWriter, r *http.Request, ws *websocket.Conn) {
+	wg.Add(1)
+	defer wg.Done()
 	var (
 		req  = NewRequest(r)
 		resp = NewResponse(w)
@@ -99,26 +105,75 @@ func Run(port int) {
 
 	runStartupHooks()
 
-	go func() {
-		time.Sleep(100 * time.Millisecond)
-		fmt.Printf("Listening on %s...\n", localAddress)
-	}()
+	listener, err := goagain.Listener()
+	if nil != err {
+		go func() {
+			time.Sleep(100 * time.Millisecond)
+			INFO.Printf("Listening on %s...\n", localAddress)
+		}()
 
-	if HttpSsl {
-		if network != "tcp" {
-			// This limitation is just to reduce complexity, since it is standard
-			// to terminate SSL upstream when using unix domain sockets.
-			ERROR.Fatalln("SSL is only supported for TCP sockets. Specify a port to listen on.")
-		}
-		ERROR.Fatalln("Failed to listen:",
-			Server.ListenAndServeTLS(HttpSslCert, HttpSslKey))
-	} else {
-		listener, err := net.Listen(network, localAddress)
+		listener, err = net.Listen(network, localAddress)
 		if err != nil {
 			ERROR.Fatalln("Failed to listen:", err)
+			return
 		}
-		ERROR.Fatalln("Failed to serve:", Server.Serve(listener))
+
+		go startServe(network, localAddress, listener)
+	} else {
+		go func() {
+			time.Sleep(100 * time.Millisecond)
+			INFO.Printf("Resuming Listening on %s...\n", localAddress)
+		}()
+
+		go startServe(network, localAddress, listener)
+
+		// Kill the parent, now that the child has started successfully.
+		if err := goagain.Kill(); nil != err {
+			ERROR.Fatalln(err)
+		}
 	}
+
+	INFO.Printf("Monitoring signals.\n")
+
+	// Block the main goroutine awaiting signals.
+	if _, err := goagain.Wait(listener); nil != err {
+		ERROR.Fatalln(err)
+	}
+
+	INFO.Printf("Closing listener.\n")
+
+	// Close the listener so we stop accepting new requests.
+	// Existing ones should still be completed.
+	if err := listener.Close(); nil != err {
+		ERROR.Fatalln(err)
+	}
+
+	INFO.Printf("Waiting for handlers to complete.\n")
+	wg.Wait()
+
+	INFO.Printf("Running Shutdown Hooks..\n")
+	runShutdownHooks()
+
+	INFO.Printf("Exit.\n")
+}
+
+func startServe(network string, localAddress string, listener net.Listener) {
+	ERROR.Fatalln("Failed to serve:", Server.Serve(listener))
+	// if HttpSsl {
+	// 	if network != "tcp" {
+	// 		// This limitation is just to reduce complexity, since it is standard
+	// 		// to terminate SSL upstream when using unix domain sockets.
+	// 		ERROR.Fatalln("SSL is only supported for TCP sockets. Specify a port to listen on.")
+	// 	}
+	// 	ERROR.Fatalln("Failed to listen:",
+	// 		Server.ListenAndServeTLS(HttpSslCert, HttpSslKey))
+	// } else {
+	// 	listener, err := net.Listen(network, localAddress)
+	// 	if err != nil {
+	// 		ERROR.Fatalln("Failed to listen:", err)
+	// 	}
+	// 	ERROR.Fatalln("Failed to serve:", Server.Serve(listener))
+	// }
 }
 
 func runStartupHooks() {
@@ -127,7 +182,14 @@ func runStartupHooks() {
 	}
 }
 
+func runShutdownHooks() {
+	for _, hook := range shutdownHooks {
+		hook()
+	}
+}
+
 var startupHooks []func()
+var shutdownHooks []func()
 
 // Register a function to be run at app startup.
 //
@@ -167,4 +229,42 @@ var startupHooks []func()
 //
 func OnAppStart(f func()) {
 	startupHooks = append(startupHooks, f)
+}
+
+// Register a function to be run at app shutdown.
+//
+// The order you register the functions will be the order they are run.
+// You can think of it as a FIFO queue.
+// This process will happen after the server has received a shutdown signal,
+// and after the server has stopped listening for connections.
+//
+// If your application spawns it's own goroutines it will also be responsible
+// for the graceful cleanup of those. Otherwise they will terminate with the main thread.
+//
+// Example:
+//
+//      // from: yourapp/app/controllers/somefile.go
+//      func ShutdownBackgroundDBProcess() {
+//          // waits for background process to complete,
+//          // or sends a signal on a termination channel.
+//      }
+//
+//      func CloseDB() {
+//          // do DB cleanup stuff here
+//      }
+//
+//      // from: yourapp/app/init.go
+//      func init() {
+//          // set up filters...
+//
+//          // register startup functions
+//          revel.OnAppShutdown(ShutdownBackgroundDBProcess)
+//          revel.OnAppShutdown(CloseDB)
+//      }
+//
+// This can be useful when you need to close client connections, files,
+// shutdown or terminate monitors and other goroutines.
+//
+func OnAppExit(f func()) {
+	shutdownHooks = append(shutdownHooks, f)
 }
